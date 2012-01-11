8210974adcd6ffbeee210b1ce438f68295d0d053
Roman romul.zurov Zhdanov <romul.zurov@gmail.com>
2012-01-12 01:07:08 +0300

    v. 0.0.2

--- src/com/github/testastaxi/TestASTaxiActivity.java
+++ src/com/github/testastaxi/TestASTaxiActivity.java
@@ -2,12 +2,29 @@ package com.github.testastaxi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class TestASTaxiActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	private EditText text;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		text = (EditText) findViewById(R.id.editText1);
+	}
+
+	public void startClick(View view) {
+		switch (view.getId()) {
+		case R.id.button_start:
+			text.setText(R.string.version);
+			break;
+		case R.id.button_stop:
+			text.setText("");
+		}
+	}
+
 }
